486f672caab3097e2cae6609f67e9d2fbae39f6a
JevonRambarran99 <46714709+JevonRambarran99@users.noreply.github.com>
2021-03-05 22:08:38 -0500

    Andrew and Jevon Not Kenta created scripts

--- phase3/autotest.sh
+++ phase3/autotest.sh
@@ -1,17 +1,53 @@
 #!/bin/bash
 
-# Author: Jevon Rambarran&Andrew
+# Author: Jevon Rambarran & Andrew Dale 
 # Objective: Scripts for passing in test file into bank terminal
 
-cd BankTesting/login
-for FILE in *.inp; do echo "Testing $FILE:";
-    ../.././main ../../CurrentBankAccounts.txt output/${FILE%.*}.atf < $FILE > output/${FILE%.*}.out
-done
+cd BankTesting
 
-for FILE in *.bto; do echo "Comparing $FILE with ${FILE%.*}.out";
-    diff $FILE output/${FILE%.*}.out
-done
+# Create array of transactions
+declare -a transactions=("login" "logout" "paybill" "transfer"
+                        "withdrawal" "changeplan" "create" "delete"
+                        "deposit" "disable")
 
-for FILE in *.etf; do echo "Comparing $FILE with ${FILE%.*}.atf";
-    diff $FILE output/${FILE%.*}.atf
+# Loop throught each transaction test directory
+for i in "${transactions[@]}"
+    do
+    echo "Testing $i transactions:";
+    cd $i
+        #Running test file to generate actual session transaction file and actual terminal output
+        for FILE in *.inp; do echo "Running test $FILE";
+            #Starting the bank terminal and generating our output files
+            ../.././main ../../CurrentBankAccounts.txt output/${FILE%.*}.atf < $FILE > output/${FILE%.*}.out
+        done
+
+        #Compare expected terminal output with actual terminal output
+        for FILE in *.bto; do echo "Comparing $FILE with ${FILE%.*}.out";
+            # Check to see if the files are identical
+            if diff -q $FILE output/${FILE%.*}.out >/dev/null
+            then
+                echo "  Test Passed";
+            else
+                echo "  Test Failed";
+                diff $FILE output/${FILE%.*}.out
+            fi
+            echo "";
+        done
+
+        #Compare expected transaction file with actual transaction file
+        for FILE in *.etf; do echo "Comparing $FILE with ${FILE%.*}.atf";
+            # Check to see if the files are identical
+            if diff -q $FILE output/${FILE%.*}.atf >/dev/null
+            then
+                echo "  Test Passed";
+            else
+                echo "  Test Failed";
+                diff $FILE output/${FILE%.*}.atf
+            fi
+            echo "";
+        done
+    echo "";
+    echo "";
+    echo "";
+    cd ../
 done
